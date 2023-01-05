587883d137b674d7c4cfa29a30620abfcf9727bf
cituk <ecituk@thefives.com.mx>
2023-01-04 23:28:15 -0500

    Counter Options Store

--- src/counter/pages/Counter1Page.vue
+++ src/counter/pages/Counter1Page.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCounterOptionsStore } from '@/store/counter-options';
 
+    const counterStore = useCounterOptionsStore() //aqui no puedo desestructurar, pierdo la reactividad si se hace
+    const {count, squareCount} = storeToRefs(counterStore) //con esto puedo desestructurar state y getters
 </script>
 
 <template>
-    
-    <h1>Counter 1 Page:</h1>
+    <h1>Pinia options</h1>
+    <h4>Counter 1 Page: {{count}}</h4>
+    <h4>Squate: {{squareCount}}</h4>
     
 </template>
-
+ 
 
 <style lang="scss" scoped>
 
