bfd23b8a549b290d9549f2f7978207d816930fbf
cituk <ecituk@thefives.com.mx>
2023-01-04 23:49:47 -0500

    Finalizar el counter options store

---
+++ src/counter/components/CounterOptionsValue.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    import { storeToRefs } from 'pinia';
+    import { useCounterOptionsStore } from '@/store/counter-options';
+
+    const counterStore = useCounterOptionsStore() //aqui no puedo desestructurar, pierdo la reactividad si se hace
+    const { count, lastChanged } = storeToRefs(counterStore) //con esto puedo desestructurar state y getters
+
+
+</script>
+
+<template>
+    <h1>El valor es: {{count}}</h1>
+    <h4>Ultimo cambio: {{lastChanged}}</h4>
+</template>
+
+
+<style scoped>
+
+</style>
--- src/counter/pages/Counter1Page.vue
+++ src/counter/pages/Counter1Page.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 import { useCounterOptionsStore } from '@/store/counter-options';
+import CounterOptionsValue from '@/counter/components/CounterOptionsValue.vue';
 
     const counterStore = useCounterOptionsStore() //aqui no puedo desestructurar, pierdo la reactividad si se hace
-    const {count, squareCount} = storeToRefs(counterStore) //con esto puedo desestructurar state y getters
+    const { count, squareCount } = storeToRefs(counterStore) //con esto puedo desestructurar state y getters
+    const { increment, incrementBy } = counterStore //desestructuro del store
 </script>
 
 <template>
     <h1>Pinia options</h1>
     <h4>Counter 1 Page: {{count}}</h4>
     <h4>Squate: {{squareCount}}</h4>
-    
+    <br>
+    <button @click="increment()">+1</button>
+    <button @click="incrementBy(2)">+2</button>
+    <button @click="incrementBy(5)">+5</button>
+    <button @click="counterStore.$reset()">Reset</button>
+    <br>
+    <CounterOptionsValue/>
+
 </template>
  
 
