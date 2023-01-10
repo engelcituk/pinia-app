fb995ab03f55952e9c2b0efcf3ded452ce8139bd
eCituk <ecituk@thefives.com.mx>
2023-01-10 08:48:15 -0500

    Cargar clientes basados en la página actual

--- src/clients/composables/useClients.ts
+++ src/clients/composables/useClients.ts
@@ -6,8 +6,8 @@ import { storeToRefs } from 'pinia';
 import type { Client } from "@/clients/interfaces/client";
 import { useClientsStore } from '@/store/clients';
 
-const getClients = async():Promise<Client[]> => {
-    const { data } = await clientsApi.get<Client[]>('clients?_page=1')
+const getClients = async(page: number):Promise<Client[]> => {
+    const { data } = await clientsApi.get<Client[]>(`clients?_page=${page}`)
     return data
 }
 
@@ -17,8 +17,8 @@ const useClients = () => {
     const { currentPage, clients, totalPages } = storeToRefs(store)
 
     const { isLoading, data } =  useQuery(
-        ['clients?page=1', 1],
-        () => getClients(),
+        ['clients?page', currentPage ],
+        () => getClients(currentPage.value),// referencia reactiva, por lo que se toma su valor con value
         
     )
 
